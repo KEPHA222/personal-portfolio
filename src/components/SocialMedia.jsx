7cd17760337b2efa7cf951913aa919483936034e
kepha222 <kephamotari1@.com>
2022-10-28 17:24:08 +0300

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BsTwitter, BsInstagram, BsFacebook } from "react-icons/bs";
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <BsFacebook />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
